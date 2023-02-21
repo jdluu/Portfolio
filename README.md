e00d9f752157f1434167d4707e145ac24c3562a8
Jeffrey Luu <83382029+jdluu@users.noreply.github.com>
2023-02-21 11:18:42 -0800

    synthesize portfolio code from class

---
+++ README.md
@@ -0,0 +1,11 @@
+# My Web Portfolio
+
+##
+
+## Part 1 - Plan Site and Semantic HTML Execution
+
+## Part 2 - Standards Based CSS Responsive Web Design
+
+## Part 3 - In Page Javascript with Site Improvements
+
+## Part 4 - Ajax and more Javascript Improvements
---
+++ scripts/theme.js
@@ -0,0 +1,26 @@
+const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
+
+//Track the theme switch button 
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
